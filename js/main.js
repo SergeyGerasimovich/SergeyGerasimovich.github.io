3a6f79ed88ceeac5b2ff49c039e674c03b1466fb
Serhii Herasymovych <serhiiherasymovych@gmail.com>
2018-04-23 17:10:11 +0300

    sixth commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 'use strict';
 
 $(document).ready(function() {
+	$('.js-section-scroll').on('click', function(e) {
+	  e.preventDefault();
+	  var section = $(this).attr('href').substr($(this).attr('href').indexOf('#'));
+	  var $section = $(section);
+
+	  $('html, body').animate({
+	    scrollTop: $section.offset().top + 'px'
+	  }, 1000);
+	});
 	$(".gallery").flipster({
 		style: 'carousel',
 	    spacing: -0.77,
