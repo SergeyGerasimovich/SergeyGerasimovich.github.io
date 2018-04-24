1267cfe918200a1c57e650d546168d9a2b54df95
Serhii Herasymovych <serhiiherasymovych@gmail.com>
2018-04-24 16:44:18 +0300

    seventh commit
    fix bug (scrolling)

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
 	     if ($(this).offset().top < fromTop)
 	       return this;
 	   });
-	   cur = cur[cur.length-1];
+	   cur = cur[cur.length-0];
 	   var id = cur && cur.length ? cur[0].id : "";
 	   if (lastId !== id) {
 	       lastId = id;
