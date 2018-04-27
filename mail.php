81ca39557d295cb83583161487537e9015c59a59
Serhii Herasymovych <serhiiherasymovych@gmail.com>
2018-04-27 14:37:52 +0300

    eightnt commit 0.0.6

--- mail.php
+++ mail.php
@@ -1,11 +1,7 @@
-<?php
+<?
+if (isset($_POST['m_data'])) {$m_data = $_POST['m_data'];}
+if (isset($_POST['toEmail'])) {$toEmail = $_POST['toEmail'];}
+if (isset($_POST['fromEmail'])) {$fromEmail = $_POST['fromEmail'];}
+if (isset($_POST['them'])) {$them = $_POST['them'];}
 
-$to = 'serhiiherasymovych@gmail.com';
-$subject = 'Partners';
-$message = $_POST['name'];
-$message .= $_POST['tel'];
-$message .= $_POST['email'];
-
-mail($to, $subject, $message);
-
-?>
+$send = mail($toEmail,$them,$m_data,"Content-type:text/plain; charset = utf-8\r\nFrom:$fromEmail");
